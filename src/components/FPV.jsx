3dd846a74d746422da4c7967279eeff2faca5c41
BGagnolo <benjamingagnolo@gmail.com>
2023-08-29 09:12:40 -0300

    FPV

--- src/components/FPV.jsx
+++ src/components/FPV.jsx
@@ -2,11 +2,5 @@ import { PointerLockControls } from '@react-three/drei'
 import { useThree } from '@react-three/fiber'
 
 export function FPV () {
-  const { camera, gl } = useThree()
-
-  return (
-    <PointerLockControls
-      args={[camera, gl.domElement]}
-    />
-  )
+ <></>
 }
